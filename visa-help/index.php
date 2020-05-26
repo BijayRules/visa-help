5cca6d87bc193d376a19cfc9a039c9943164b33f
BijayRules <bijayshrestha01238@gmail.com>
2020-05-26 19:11:28 +0545

    Add files via upload

---
+++ visa-help/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- UIKIT CSS -->
+  <link rel="stylesheet" href="css/uikit.min.css">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet"> 
+
+  <title>Visa Help</title>
+</head>
+<body>
+  <header class="uk-position-relative">
+    <div class="header-bg">
+      <img src="images/visa-help-background.png" alt="Visa Help Background">
+    </div>
+    <div class="uk-container">
+      <!-- Navigation Bar -->
+      <nav class="navigation uk-flex uk-flex-between">
+            <a class="logo" href="#">
+              <img src="images/visa-help-logo.png" alt="Visa Help">
+            </a>
+            <div class="menu-wrapper">
+              <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Studyaboard</a></li>
+                  <li><a href="#">services</a></li>
+                  <li><a href="#">testimonial</a></li>
+                  <li><a href="#">contact</a></li>
+              </ul>
+            </div>
+      </nav>
+    </div>
+
+    <div class="uk-container uk-flex uk-flex-between">
+      <div class="info-text uk-light">
+        <span class="title-font">
+          World Class Australia 
+          <strong>
+          Universities & Colleges</strong>
+          <small>
+            We offer a complet  to Z approach to overseas education, without the complications associated with the international admissions process.
+          </small>
+          <div class="uk-button">
+            <a href="#">Read More</a>
+          </div>
+        </span>
+      </div>
+      <div class="teacher-background">
+        <img src="images/teachers.png" alt="Teachers">
+      </div>
+    </div>
+  </header>
+
+
+</body>
+</html>

bcd51dda99359e9cf45f28ea36537e622e37c906
BijayRules <bijayshrestha01238@gmail.com>
2020-05-28 17:10:52 +0545

    Add files via upload

--- visa-help/index.php
+++ visa-help/index.php
@@ -15,16 +15,20 @@
   <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet"> 
 
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/ef2dde7794.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
   <title>Visa Help</title>
 </head>
 <body>
-  <header class="uk-position-relative">
+  <header class="header uk-position-relative">
     <div class="header-bg">
       <img src="images/visa-help-background.png" alt="Visa Help Background">
     </div>
-    <div class="uk-container">
+    <div class="navigation uk-container">
       <!-- Navigation Bar -->
-      <nav class="navigation uk-flex uk-flex-between">
+      <nav class="uk-flex uk-flex-between">
             <a class="logo" href="#">
               <img src="images/visa-help-logo.png" alt="Visa Help">
             </a>
@@ -39,28 +43,175 @@
               </ul>
             </div>
       </nav>
-    </div>
 
-    <div class="uk-container uk-flex uk-flex-between">
-      <div class="info-text uk-light">
-        <span class="title-font">
-          World Class Australia 
-          <strong>
-          Universities & Colleges</strong>
-          <small>
-            We offer a complet  to Z approach to overseas education, without the complications associated with the international admissions process.
-          </small>
-          <div class="uk-button">
-            <a href="#">Read More</a>
-          </div>
-        </span>
-      </div>
-      <div class="teacher-background">
-        <img src="images/teachers.png" alt="Teachers">
+      <!-- Visa Help Information -->
+      <div class="info-text uk-container uk-flex uk-flex-between">
+        <div class="uk-light">
+          <span class="title-font">
+            World Class Australia 
+            <strong>
+            Universities & Colleges</strong>
+            <small>
+              We offer a complete  to Z approach to overseas education, without the complications associated with the international admissions process.
+            </small>
+            <div class="uk-button">
+              <a href="#">Read More</a>
+            </div>
+          </span>
+        </div>
+        <div class="teacher-background">
+          <img src="images/teachers.png" alt="Teachers">
+        </div>
       </div>
     </div>
   </header>
+  
+  <!-- About Us Section -->
+  <section class="aboutus">
+    <div class="uk-container uk-flex uk-flex-between">
+      <div class="aboutus__text">
+        <div class="aboutus--text">
+          <span class="aboutus-title uk-text-center">About Us</span>
+          <p>We offer a complete A to Z approach to over- <br> seas education, without the complications as- <br> sociated with the international admissions process.
+          </p>
+          <a href="#" class="uk-button">Read More</a>
+        </div>
+      </div>
+      <div class="about-org uk-position-relative">        
+        <img src="images/student.png" alt="Student">
+      </div>
+    </div>
+    <div class="uk-container">
+      <div class="uk-flex uk-flex-between">
+        <div class="achievements">
+          <div class="achievement-icon uk-flex">
+            <img src="images/achievement-icon.png" alt="Achievement Icon">
+            <span class="achievement-num">
+              <strong>1000</strong>
+              <small>Successful Visa Grant</small>
+            </span>
+          </div>
+        </div><div class="achievements">
+          <div class="achievement-icon uk-flex">
+            <img src="images/achievement-icon.png" alt="Achievement Icon">
+            <span class="achievement-num">
+              <strong>1000</strong>
+              <small>College Admission</small>
+            </span>
+          </div>
+        </div>
+        <div class="achievements">
+          <div class="achievement-icon uk-flex">
+            <img src="images/achievement-icon.png" alt="Achievement Icon">
+            <span class="achievement-num">
+              <strong>1000</strong>
+              <small>Professional Year</small>
+            </span>
+          </div>
+        </div>
+        <div class="achievements">
+          <div class="achievement-icon uk-flex">
+            <img src="images/achievement-icon.png" alt="Achievement Icon">
+            <span class="achievement-num">
+              <strong>1000</strong>
+              <small>Client Satisfaction</small>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
+  <!-- Migration Services Section -->
+  <section class="migration-services">
+    <div class="migration-services-info">
+      <h1 class="info-title uk-text-uppercase">Migration Services</h1>
+      <p class="migration-text">We offer a complete A to Z approach to overseas education, without the complications associated with the <br> international admissions process.
+      </p>
+    </div>
+
+    <div class="uk-container uk-flex uk-flex-around uk-flex-wrap">
+      <div class="migration-icons active uk-width-1-3">
+        <i class="fas fa-user-cog"></i>
+        <span class="migration-icon-text">
+          <strong class="migration-icon-title">Proper Counselling</strong>
+          <small class="migration-icon-info">Our counseling team are experts and <br> willing to answer your queries with best <br> options.</small>
+        </span>
+      </div>
+      <div class="migration-icons uk-width-1-3">
+        <i class="fas fa-user-cog"></i>
+        <span class="migration-icon-text">
+          <strong class="migration-icon-title">Finding Scholarship</strong>
+          <small class="migration-icon-info">We help our students to get scholarship <br> available from various choice of universi- <br> ties.</small>
+        </span>
+      </div>
+      <div class="migration-icons uk-width-1-3">
+        <i class="fas fa-users"></i>
+        <span class="migration-icon-text">
+          <strong class="migration-icon-title">Future Assurance</strong>
+          <small class="migration-icon-info">Case handled by our team are out of any <br> doubts assuring your best in future.</small>
+        </span>
+      </div>
+      <div class="migration-icons uk-width-1-3">
+        <i class="fas fa-user-cog"></i>
+        <span class="migration-icon-text">
+          <strong class="migration-icon-title">Financial Advising</strong>
+          <small class="migration-icon-info">We guide student's for saving and help <br> them to determine best fit career choice.</small>
+        </span>
+      </div>
+
+      <div class="migration-icons uk-width-1-3">
+        <i class="fas fa-user-cog"></i>
+        <span class="migration-icon-text">
+          <strong class="migration-icon-title">Assured Success</strong>
+          <small class="migration-icon-info">We value for success and our years of <br> experience have made us robust.</small>
+        </span>
+      </div>
+      <div class="migration-icons uk-width-1-3">
+        <i class="fas fa-users"></i>
+        <span class="migration-icon-text">
+          <strong class="migration-icon-title">Result Oriented</strong>
+          <small class="migration-icon-info">We work genuinely focusing on the <br> career of students with priority to time and <br> success ratio.</small>
+        </span>
+      </div>
+    </div>
+  </section>
+
+  <!-- Testimonial Section -->
+  <section class="testimonials">
+    <div class="testimonials-info uk-text-center">
+      <h1 class="info-title uk-text-uppercase">Student Testimonials</h1>
+    </div>
+    <div class="testimonial-card uk-container uk-flex uk-flex-around">
+      <div class="uk-card uk-card-default">
+        <div class="uk-card-body">
+          <p class="testimonial-text">
+          I am text block. Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+          </p>
+        </div>
+      </div>
+      <div class="uk-card uk-card-default">
+        <div class="uk-card-body">
+          <p class="testimonial-text">
+          I am text block. Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+          </p>
+        </div>
+      </div>
+      <div class="uk-card uk-card-default">
+        <div class="uk-card-body">
+          <p class="testimonial-text">
+          I am text block. Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+        </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
 
 </body>
 </html>
